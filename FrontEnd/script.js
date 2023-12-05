4501d05b2408a0450d2da42624fc9629abe64ba5
AliceSay <alicesay@hotmail.fr>
2023-12-05 14:06:54 +0100

    gallery filtres fonctionnelle

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,87 +1,49 @@
-// récupération des travaux
-
 const gallery = document.getElementById('gallery')
-
 const urlWorks = 'http://localhost:5678/api/works'
 
 const getProjects = () => {
   fetch(urlWorks)
-    .then(function (res) {
-      return res.json()
-    })
-
-    .then(function (data) {
-      for (element of data) {
-        gallery.innerHTML += `<figure data-category="${element.categoryId}">
-                                 <img src="${element.imageUrl}" alt="${element.imageUrl}">
-                                 <figcaption>${element.title}</figcaption>
-                               </figure>`
-      }
+    .then((res) => res.json())
+    .then((data) => {
+      gallery.innerHTML = data
+        .map(
+          (element) => `
+        <figure class="gallery-item" data-category="${element.categoryId}">
+          <img src="${element.imageUrl}" alt="${element.imageUrl}">
+          <figcaption>${element.title}</figcaption>
+        </figure>`
+        )
+        .join('')
     })
 }
 
-getProjects()
+const filterFiguresByCategory = (category) => {
+  const figures = Array.from(gallery.querySelectorAll('.gallery-item'))
 
-// tri des travaux
-
-// BOUTON OBJETS
-
-const btnObjects = document.querySelector('.btn-objects')
-btnObjects.addEventListener('click', () => {
-  const figures = Array.from(gallery.querySelectorAll('#gallery figure'))
-  const figuresObjects = figures.filter(function (figure) {
-    if (figure.getAttribute('data-category') === '1') {
-      return true
+  figures.forEach((figure) => {
+    const figureCategory = figure.getAttribute('data-category')
+    if (figureCategory === category || category === 'all') {
+      figure.classList.remove('hidden')
     } else {
-      return false
+      figure.classList.add('hidden')
     }
   })
-  gallery.innerHTML = ''
-  figuresObjects.forEach((figure) => {
-    gallery.appendChild(figure)
-  })
-})
+}
 
-// BOUTON APPARTEMENTS
-
-const btnAppts = document.querySelector('.btn-appartements')
-btnAppts.addEventListener('click', () => {
-  const figures = Array.from(gallery.querySelectorAll('#gallery figure'))
-  const figuresAppts = figures.filter(function (figure) {
-    if (figure.getAttribute('data-category') === '2') {
-      return true
-    } else {
-      return false
-    }
-  })
-  gallery.innerHTML = ''
-  figuresAppts.forEach((figure) => {
-    gallery.appendChild(figure)
-  })
-})
-
-// BOUTON HOTELS&RESAURANTS
-
-const btnHotelsAndRests = document.querySelector('.btn-hotels-restaurants')
-btnHotelsAndRests.addEventListener('click', () => {
-  const figures = Array.from(gallery.querySelectorAll('#gallery figure'))
-  const figuresHotelsAndRests = figures.filter(function (figure) {
-    if (figure.getAttribute('data-category') === '3') {
-      return true
-    } else {
-      return false
-    }
-  })
-  gallery.innerHTML = ''
-  figuresHotelsAndRests.forEach((figure) => {
-    gallery.appendChild(figure)
-  })
-})
-
-// BOUTON ALL
-
-const btnAll = document.querySelector('.btn-all')
-btnAll.addEventListener('click', () => {
-  gallery.innerHTML = ''
+document.addEventListener('DOMContentLoaded', () => {
   getProjects()
+
+  document
+    .querySelector('.btn-objects')
+    .addEventListener('click', () => filterFiguresByCategory('1'))
+  document
+    .querySelector('.btn-appartements')
+    .addEventListener('click', () => filterFiguresByCategory('2'))
+  document
+    .querySelector('.btn-hotels-restaurants')
+    .addEventListener('click', () => filterFiguresByCategory('3'))
+
+  document
+    .querySelector('.btn-all')
+    .addEventListener('click', () => filterFiguresByCategory('all'))
 })
